132cd758da7b7384986d680706cdd53e58951a62
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-05-20 15:26:33 +0200

    update4.1

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const http = require('http')
 const server = http.createServer(app)
 app.use(cors({
     origin : process.env.mode ==='pro' ? [ process.env.
-        client_customer_production_url,] : ['http://localhost:3000',
+        client_customer_production_url,process.env.
+        client_admin_production_url] : ['http://localhost:3000',
             'http://localhost:3001'],
     credentials: true
 }))
@@ -18,7 +19,8 @@ app.use(cors({
 const io = socket(server, {
     cors: {
         origin: process.env.mode ==='pro' ? [process.env.
-            client_customer_production_url,] : ['http://localhost:3000',
+            client_customer_production_url,process.env.
+        client_admin_production_url] : ['http://localhost:3000',
                 'http://localhost:3001'],
         credentials: true
     }
