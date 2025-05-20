0d6e2bee678880407e9dbc2600512eb46d4d8242
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-05-20 15:44:46 +0200

    solved

--- server.js
+++ server.js
@@ -8,23 +8,35 @@ const { dbConnect } = require('./utiles/db')
 const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
-app.use(cors({
-    origin : process.env.mode ==='pro' ? [ process.env.
+
+const allowedOrigins = process.env.mode == 'pro'? [ process.env.
         client_customer_production_url,process.env.
         client_admin_production_url] : ['http://localhost:3000',
-            'http://localhost:3001'],
-    credentials: true
+            'http://localhost:3001'];
+
+app.use(cors({
+origin : function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)){
+        callback(null,true);
+    }else {
+        callback(new Error("Not allowed by CORS"));
+    }
+},
+credentials: true
 }))
 
 const io = socket(server, {
     cors: {
-        origin: process.env.mode ==='pro' ? [process.env.
-            client_customer_production_url,process.env.
-        client_admin_production_url] : ['http://localhost:3000',
-                'http://localhost:3001'],
+        origin : function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)){
+                callback(null,true);
+            }else {
+                callback(new Error("Not allowed by CORS"));
+             }
+        },
         credentials: true
-    }
-})
+    },
+});
 
 var allCustomer = []
 var allSeller = []
