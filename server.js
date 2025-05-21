6226d289e3bff16f3e0d02dc1fc4c5f80d65c782
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-05-21 13:49:38 +0200

    frustrated

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const http = require('http')
 const server = http.createServer(app)
 
 const allowedOrigins = process.env.mode == 'pro'? [process.env.client_customer_production_url,
-    process.env.client_admin_production_url] : ['https://www.nimbo.co.zw',
-            'https://nimbo-dashboard.vercel.app/', ];
+    process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001'];
 
 app.use(cors({
 origin: function (origin, callback) {
