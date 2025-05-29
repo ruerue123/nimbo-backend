1cdde6b42505ee4bc3061d587e887c123852ee94
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-05-29 13:38:55 +0200

    hopefu;;y solved

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 
-const allowedOrigins = process.env.mode === 'pro'? [process.env.client_customer_production_url,
-    process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001'];
+const allowedOrigins = process.env.mode ==='pro'? [process.env.client_customer_production_url, process.env.client_admin_production_url]
+: ['http://localhost:3000', 'http://localhost:3001'];
 
 app.use(cors({
 origin: function (origin, callback) {
