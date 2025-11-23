e1699d6f7c9f4b54c8a67c7808135a0de5aa5e1d
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-11-23 14:28:18 +0200

    chat

--- models/authOrder.js
+++ models/authOrder.js
@@ -9,30 +9,47 @@ const authSchema = new Schema({
         type: Schema.ObjectId,
         required : true
     },
+    customerId: {
+        type: Schema.ObjectId,
+        required : false
+    },
+    customerName: {
+        type: String,
+        required : false
+    },
     products: {
         type: Array,
-        required : true  
-    }, 
+        required : true
+    },
     price: {
         type: Number,
-        required : true  
-    },     
+        required : true
+    },
     payment_status: {
         type: String,
-        required : true  
+        required : true
     },
     shippingInfo: {
         type: String,
-        required : true  
+        required : true
     },
     delivery_status: {
         type: String,
-        required : true  
+        required : true
     },
     date: {
         type: String,
         required : true
-    } 
+    },
+    // Delivery details added by seller
+    deliveryDetails: {
+        courierName: { type: String, default: '' },
+        courierPhone: { type: String, default: '' },
+        estimatedDate: { type: String, default: '' },
+        estimatedTime: { type: String, default: '' },
+        trackingNumber: { type: String, default: '' },
+        notes: { type: String, default: '' }
+    }
 },{ timestamps: true })
 
 module.exports = model('authorOrders',authSchema)
